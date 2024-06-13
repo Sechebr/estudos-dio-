679bd2147ffb9b0e80197115185e540d7d9a2eed
Filipe H. Secherini <104800064+Sechebr@users.noreply.github.com>
2024-06-13 10:29:43 -0300

    Add files via upload

---
+++ dio/teste_dio.js
@@ -0,0 +1,22 @@
+let nomeHero = "Filipe"
+let nivelHero = 1000
+
+
+
+if (nivelHero <= 1000){
+    console.log("O Heroi"+" "+nomeHero+" "+"está no nivel ferro")
+}else if ((nivelHero > 1000) && (nivelHero <= 2000)){
+    console.log("O Heroi"+" "+nomeHero+" "+"está no nivel Bronze")
+}else if ((nivelHero > 2000) && (nivelHero <= 3000)){
+    console.log("O Heroi"+" "+nomeHero+" "+"está no nivel Prata")
+}else if ((nivelHero > 3000) && (nivelHero <= 4000)){
+  console.log("O Heroi"+" "+nomeHero+" "+"está no nivel Ouro")
+}else if ((nivelHero > 4000) && (nivelHero <= 5000)){
+    console.log("O Heroi"+" "+nomeHero+" "+"está no nivel Platina")
+}else if ((nivelHero > 5000) && (nivelHero <= 6000)){
+    console.log("O Heroi"+" "+nomeHero+" "+"está no nivel Diamante")
+}else if ((nivelHero > 6000) && (nivelHero <= 7000)){
+  console.log("O Heroi"+" "+nomeHero+" "+"está no nivel Ascendente")
+}else if (nivelHero > 7000){
+  console.log("O Heroi"+" "+nomeHero+" "+"está no nivel Radiante")
+}
